e38d96c5069c85455415bc4eda6f0e1672e76c07
apontarelli <antoniopontarelli@gmail.com>
2025-01-19 13:28:43 -0800

    Add markdown image and link extraction function and tests

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -18,5 +19,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             remaining_text = after
     return new_nodes 
 
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
 
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
 
--- src/test_helper_functions.py
+++ src/test_helper_functions.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from helper_functions import split_nodes_delimiter
+from helper_functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestSplitNodesDelimiter(unittest.TestCase):
     def test_single_text_node(self):
@@ -51,6 +51,62 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         pass
         # to implement with nesting logic
         # self.assertEqual(result, expected)
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_single_image(self):
+        text = "This ![amazing image](https://image.url) is an amazing image"
+        result = extract_markdown_images(text)
+        expected = [("amazing image", "https://image.url")]
+        self.assertEqual(result, expected)
+
+    def test_multiple_images(self):
+        text = "This is first ![first image](https://first_image.url) and second ![second image](https://second_image.url) image in the string."
+        result = extract_markdown_images(text)
+        expected = [
+                ("first image", "https://first_image.url"),
+                ("second image", "https://second_image.url")
+                ]
+        self.assertEqual(result, expected)
+
+    def test_image_with_exclamation(self):
+        text = "This ![amazing image](https://image.url) is an amazing image!"
+        result = extract_markdown_images(text)
+        expected = [("amazing image", "https://image.url")]
+        self.assertEqual(result, expected)
+
+    def test_image_with_no_alt_text(self):
+        text = "This ![](https://image.url) is a boring image!"
+        result = extract_markdown_images(text)
+        expected = [("", "https://image.url")]
+        self.assertEqual(result, expected)
+
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_single_links(self):
+        text = "This [amazing link](https://link.url) is an amazing image"
+        result = extract_markdown_links(text)
+        expected = [("amazing link", "https://link.url")]
+        self.assertEqual(result, expected)
+
+    def test_multiple_links(self):
+        text = "This is first [first link](https://first_link.url) and second [second link](https://second_link.url) link in the string."
+        result = extract_markdown_links(text)
+        expected = [
+                ("first link", "https://first_link.url"),
+                ("second link", "https://second_link.url")
+                ]
+        self.assertEqual(result, expected)
+
+    def test_link_with_parenthesis(self):
+        text = "This [amazing link](https://link.url) is an amazing (but simple) link!"
+        result = extract_markdown_links(text)
+        expected = [("amazing link", "https://link.url")]
+        self.assertEqual(result, expected)
+
+    def test_link_with_brackets(self):
+        text = "This [another link](https://link.url) is a simple image [with some brackets]!"
+        result = extract_markdown_links(text)
+        expected = [("another link", "https://link.url")]
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
