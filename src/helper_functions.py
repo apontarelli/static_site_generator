caa0c1ebfda16b0a29bee7769fca61ae723fb467
apontarelli <antoniopontarelli@gmail.com>
2025-01-21 01:26:26 -0800

    Add markdown_to_html method and tests; refactored methods to fix breaking tests

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,4 +1,5 @@
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from htmlnode import ParentNode
 import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -74,7 +75,7 @@ def extract_markdown_links(text):
     return matches
 
 def markdown_to_blocks(markdown):
-    split_blocks = markdown.strip().split('\n\n')
+    split_blocks = markdown.split('\n\n')
     stripped_blocks = list(map(str.strip, split_blocks))
     blocks = list(filter(None, stripped_blocks))
     return blocks
@@ -98,14 +99,86 @@ def block_to_block_type(markdown):
     if re.match(r"^[*-] ", markdown, re.MULTILINE):
         return "unordered_list"
     lines = markdown.split("\n")
-    expected_number = 1
-    for line in lines: 
-        if re.match(rf"^{expected_number}\. ", line):
-            expected_number += 1
-            continue
-        else:
-            break
-    else:
+    stripped_lines = [line.strip() for line in lines]
+    if stripped_lines[0].startswith("1. "):
+        i = 1
+        for line in stripped_lines:
+            if not line.startswith(f"{i}. "):
+                return "paragraph"
+            i += 1
         return "ordered_list"
-    return "paragraph"  
+    return "paragraph"
+
+def process_block(block):
+    block_type = block_to_block_type(block)
+    match block_type:
+        case "heading":
+            n = len(re.findall(r'#', block))
+            content = re.sub(r"^#{1,6} ", "", block) 
+            node = ParentNode(tag = f"h{n}", children = text_to_children(content))
+            return node
+
+        case "code":
+            content = re.sub(r"^```[\n]?", "", block)
+            content = re.sub(r"[\n]?```$", "", content)
+            node = ParentNode(
+                tag = "pre",
+                children = [ParentNode(tag = "code", children = text_to_children(content))])
+            return node
+
+        case "quote":
+            lines = block.split("\n")
+            cleaned_lines = []
+            for line in lines:
+                cleaned_line = re.sub(r"^>[ ]?", "", line.strip())
+                cleaned_lines.append(cleaned_line)
+            content = " ".join(cleaned_lines)
+            node = ParentNode(tag = "blockquote", children = text_to_children(content))
+            return node
+            return node
+
+        case "unordered_list":
+            lines = block.split("\n")
+            child_nodes = []
+            for line in lines:
+                content = re.sub(r"^[*-] ", "", line.strip())
+                child_node = ParentNode(tag = "li", children = text_to_children(content))
+                child_nodes.append(child_node)
+            node = ParentNode(tag = "ul", children = child_nodes)
+            return node
+
+        case "ordered_list":
+            lines = block.split("\n")
+            child_nodes = []
+            for line in lines:
+                content = re.sub(r"^\d+\. ", "", line.strip())
+                child_node = ParentNode(tag = "li", children = text_to_children(content))
+                child_nodes.append(child_node)
+            node = ParentNode(tag = "ol", children = child_nodes)
+            return node
+
+        case "paragraph":
+            content = " ".join(line.strip() for line in block.split("\n"))
+            node = ParentNode(tag = "p", children = text_to_children(content))
+            node = ParentNode(tag = "p", children = text_to_children(content))
+            return node
+    raise ValueError("Invalid block type")
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    nodes = []
+    for node in text_nodes:
+        html_node = text_node_to_html_node(node)
+        nodes.append(html_node)
+    return nodes
+
+def markdown_to_html_node(markdown):
+       # INPUT: full markdown document
+    # OUTPUT: HTMLNode with nested nodes
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for item in blocks:
+        children.append(process_block(item))
+    html = ParentNode("div", children)
+    return html
 
--- src/test_helper_functions.py
+++ src/test_helper_functions.py
@@ -9,8 +9,12 @@ from helper_functions import (
         split_nodes_link,
         text_to_textnodes,
         markdown_to_blocks,
-        block_to_block_type
+        block_to_block_type,
+        text_to_children,
+        process_block,
+        markdown_to_html_node
 )
+from htmlnode import LeafNode, ParentNode
 
 class TestHelperFunctions(unittest.TestCase):
     def test_single_text_node(self):
@@ -388,5 +392,162 @@ class TestHelperFunctions(unittest.TestCase):
         expected = "paragraph"
         self.assertEqual(result, expected)
 
+    def test_text_to_children(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result = text_to_children(text)
+        expected = [
+            LeafNode(None, "This is "),
+            LeafNode("b", "text"),
+            LeafNode(None, " with an "),
+            LeafNode("i", "italic"),
+            LeafNode(None, " word and a "),
+            LeafNode("code", "code block"),
+            LeafNode(None, " and an "),
+            LeafNode("img", "", {
+                    "src": "https://i.imgur.com/fJRm4Vk.jpeg",
+                    "alt": "obi wan image"}),
+            LeafNode(None, " and a "),
+            LeafNode("a", "link", {"href":"https://boot.dev"}),
+        ]
+        self.assertEqual(result, expected)
+
+    def test_process_heading(self):
+        text = "# This is a heading"
+        result = process_block(text)
+        expected = ParentNode(
+            "h1",
+            [LeafNode(None, "This is a heading")])
+        self.assertEqual(result, expected)
+
+    def test_process_code(self):
+        text = "```This is some code\nthis is another line of code \nthe most code```"
+        result = process_block(text)
+        expected = ParentNode(
+            "pre",
+            [ParentNode(tag = "code", children = [
+                LeafNode(None, "This is some code\nthis is another line of code \nthe most code")
+                ])])
+        self.assertEqual(result, expected)
+
+    def test_process_quote(self):
+        text = ">This is a quote\n>The quote continues on\n>will this person ever stop talking"
+        result = process_block(text)
+        expected = ParentNode(
+            tag = "blockquote",
+            children = [LeafNode(None, "This is a quote The quote continues on will this person ever stop talking")])
+        self.assertEqual(result, expected)
+
+    def test_process_unordered_list(self):
+        text = "- This is an unordered list\n* another list item with an *\n- last item i promise"
+        result = process_block(text)
+        expected = ParentNode(
+            tag = "ul",
+            children = [
+                ParentNode("li", children = [LeafNode(None, "This is an unordered list")]),
+                ParentNode("li", children = [LeafNode(None, "another list item with an *")]),
+                ParentNode("li", children = [LeafNode(None, "last item i promise")])])
+        self.assertEqual(result, expected)
+
+    def test_process_ordered_list(self):
+        text = "1. This is the first item\n2. This is the second item\n3. This is the last item"
+        result = process_block(text)
+        expected = ParentNode(
+            tag = "ol",
+            children = [
+                ParentNode("li", children = [LeafNode(None, "This is the first item")]),
+                ParentNode("li", children = [LeafNode(None, "This is the second item")]),
+                ParentNode("li", children = [LeafNode(None, "This is the last item")])])
+        self.assertEqual(result, expected)
+
+    def test_process_paragraph(self):
+        text = "1. This is the first item\n* This is the second item\n3. This is the last item"
+        result = process_block(text)
+        expected = ParentNode(
+            tag = "p",
+            children = [LeafNode(None, "1. This is the first item * This is the second item 3. This is the last item")])
+        self.assertEqual(result, expected)
+
+    def test_paragraph(self):
+        md = """
+        This is **bolded** paragraph
+        text in a p
+        tag here
+
+        """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
+        )
+
+    def test_paragraphs(self):
+        md = """
+        This is **bolded** paragraph
+        text in a p
+        tag here
+
+        This is another paragraph with *italic* text and `code` here
+
+        """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_lists(self):
+        md = """
+        - This is a list
+        - with items
+        - and *more* items
+
+        1. This is an `ordered` list
+        2. with items
+        3. and more items
+
+        """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
+        )
+
+    def test_headings(self):
+        md = """
+        # this is an h1
+
+        this is paragraph text
+
+        ## this is an h2
+        """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>this is an h1</h1><p>this is paragraph text</p><h2>this is an h2</h2></div>",
+        )
+
+    def test_blockquote(self):
+        md = """
+        > This is a
+        > blockquote block
+
+        this is paragraph text
+
+        """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
+        )
 if __name__ == "__main__":
     unittest.main()
