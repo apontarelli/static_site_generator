c7fa7af8586ef10c588025ab5b990afe0b7b83dd
apontarelli <antoniopontarelli@gmail.com>
2025-01-22 00:37:30 -0800

    Add generate page method and template/markdown files

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,4 +1,7 @@
 import re
+import os
+
+from block_markdown import markdown_to_html_node
 
 def extract_title(markdown):
     match = re.search(r"^#\s+([^#].*)$", markdown, flags=re.M)
@@ -7,5 +10,18 @@ def extract_title(markdown):
     return match.group(1).strip()
     
 def generate_page(from_path, template_path, dest_path):
-    pass
+    print(f"Generating page {from_path} to {dest_path} using {template_path}")
+    with open(from_path, 'r') as f:
+        markdown = f.read()
+    with open(template_path, 'r') as f:
+        template = f.read()
+    title = extract_title(markdown)
+    html = markdown_to_html_node(markdown).to_html()
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    dest_dir = os.path.dirname(dest_path)
+    os.makedirs(dest_dir, exist_ok=True)
+    with open(dest_path, "w") as file:
+        print(template, file=file)
+
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from textnode import TextNode
-from textnode import TextType
 from utils import copy_src_to_dest
+from generate_page import generate_page
 
 def main():
     copy_src_to_dest("./static", "./public")
+
+    content = "content/index.md" 
+    template = "template.html"
+    dest = "public/index.html"
+    generate_page(content, template, dest)
 main()
